ce93957a14ddeeb66b8ca62a5158532a19478b0f
jl052 <98014687+jl052@users.noreply.github.com>
2022-12-16 22:29:08 +0000

    Update MS Reward PC Search Spam.py

--- MS Reward PC Search Spam.py
+++ MS Reward PC Search Spam.py
@@ -1,5 +1,8 @@
 ## Enjoy your Microsoft Search Rewards Point
 ## By Jason L Nov 2021
+from re import M, T
+
+
 class reward():
 	def __init__(self):
 		self.website="https://www.bing.com/search?q="
@@ -13,12 +16,49 @@ class reward():
 					webbrowser.BackgroundBrowser("C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"))  ## change to where your msedge.exe locate
 			webbrowser.get('edge').open(target)
 			sleep(1)
+
+	def chrome_mobile_spam(self,i): #"C:\Program Files\Google\Chrome\Application\chrome.exe"
+		target="this.document.location = 'http://www.bing.com/search?q="
+		keyword="gbp to usd'"
+		webbrowser.register('chrome',
+					None,
+					webbrowser.BackgroundBrowser("C:\Program Files\Google\Chrome\Application\chrome.exe"))  ## change to where your msedge.exe locate
+		webbrowser.get('chrome').open('http://www.bing.com')
+		sleep(10)
+		#driver = webdriver.Chrome(executable_path=r'C:\Program Files\Google\Chrome\Application\chrome.exe')
+		pyautogui.press("F12")
+		sleep(10)
+
+		for j in range(i):
+			target2=target+str(int(random.random()*1000))+keyword
+			for x in list(target2):
+				pyautogui.press(x)
+			pyautogui.press('Enter')
+			sleep(0.1)
+		sleep(5)
+
+		os.system("taskkill /im chrome.exe /f")
 		os.system("taskkill /im msedge.exe /f")
+#		driver=webdriver.Edge('C:\Users\\Jason\\Desktop\\App\\Virtual Harddisk\\File Exchange\\edgedriver_win64') #C:\Users\Jason\Desktop\App\chromedriver_win32
+#		driver=webdriver.Chrome('C:\\Users\\Jason\\Desktop\\App\\chromedriver_win32')
+#		driver.get('https://www.bing.com/')
+#		driver.execute_script("this.document.location = 'http://www.bing.com/search?q=1 gbp to usd'")
+#
+#		webbrowser.register('edge',
+#					None,
+#					webbrowser.BackgroundBrowser("C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"))  ## change to where your msedge.exe locate
+#		webbrowser.get('edge').open("https://www.bing.com/search?q=tsla")
+#		sleep(1000)
 
 import webbrowser, random, os
+from selenium import webdriver
 from time import sleep 
+import pyautogui
 i=reward()
 i.edge_pc_search_spam(40)
+sleep(5)
+i.chrome_mobile_spam(30)
+
 
 
 
